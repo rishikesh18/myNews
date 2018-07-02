90c648ebb0bd649c769a31e03987e3371ba18efc
rishikesh18 <rishi.neupane@gmail.com>
2018-07-02 18:14:25 -0400

    link made to open in new tab

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<strong>शिर्षक(Headlines):</strong> " + data[i].title + "<br />" + "<strong>सारांश(Summary):</strong> " + data[i].summary + "<br />" +"<strong>लिंक(Link):</strong> " + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<strong>शिर्षक(Headlines):</strong> " + data[i].title + "<br />" + "<strong>सारांश(Summary):</strong> " + data[i].summary + "<br />" +"<strong>लिंक(Link):</strong><a href = ' " + data[i].link + "' target ='_blank' </a>"+data[i].link+"</p>");
   }
 });
 
